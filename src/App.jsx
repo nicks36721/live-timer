b01c377c579e21a8e65e45ac97e6b444c5b6a4f7
nikita gupta <nikitakeshri000@gmail.com>
2024-10-03 13:05:10 +0530

    live timer

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// timer 
+
+ import { useState } from "react"; 
+  const App =()=>{
+
+    const [time, settime]= useState(new Date().toLocaleTimeString());
+    setInterval(() =>{
+      settime(new Date().toLocaleTimeString());
+    },1000);
+    
+      return(
+        <div>
+          <h1 className="text-center mt-10 text-5xl">{time}</h1>
+        </div>
+      );
+  };
+
+  export default App;
